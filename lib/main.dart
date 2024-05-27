a8decd869998c94b12c65efdb6f46f1b99e49f17
Khanh Lee <lee.cois.dev@gmail.com>
2024-05-27 16:14:12 +0700

    fix: ci/cd

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:hive/hive.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:outfit4rent/app.dart';
+import 'package:outfit4rent/firebase_options.dart';
 import 'package:path_provider/path_provider.dart';
 
 /// Try using const constructors as much as possible!
@@ -13,6 +15,10 @@ import 'package:path_provider/path_provider.dart';
 void main() async {
   /// Initialize packages
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await EasyLocalization.ensureInitialized();
   if (Platform.isAndroid) {
     await FlutterDisplayMode.setHighRefreshRate();
--- lib/utils/exceptions/firebase_auth_exceptions.dart
+++ lib/utils/exceptions/firebase_auth_exceptions.dart
@@ -49,12 +49,8 @@ class TFirebaseAuthException implements Exception {
         return 'The action code is missing. Please provide a valid action code.';
       case 'user-token-expired':
         return 'The user\'s token has expired, and authentication is required. Please sign in again.';
-      case 'user-not-found':
-        return 'No user found for the given email or UID.';
       case 'invalid-credential':
         return 'The supplied credential is malformed or has expired.';
-      case 'wrong-password':
-        return 'The password is invalid. Please check your password and try again.';
       case 'user-token-revoked':
         return 'The user\'s token has been revoked. Please sign in again.';
       case 'invalid-message-payload':
@@ -83,14 +79,10 @@ class TFirebaseAuthException implements Exception {
         return 'The provided Cordova configuration is invalid.';
       case 'app-deleted':
         return 'This instance of FirebaseApp has been deleted.';
-      case 'user-disabled':
-        return 'The user account has been disabled.';
       case 'user-token-mismatch':
         return 'The provided user\'s token has a mismatch with the authenticated user\'s user ID.';
       case 'web-storage-unsupported':
         return 'Web storage is not supported or is disabled.';
-      case 'invalid-credential':
-        return 'The supplied credential is invalid. Please check the credential and try again.';
       case 'app-not-authorized':
         return 'The app is not authorized to use Firebase Authentication with the provided API key.';
       case 'keychain-error':
